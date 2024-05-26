249b4465c56d5bef86c7a12cf604e78737beb2e2
gauthiii <gauvij.99@gmail.com>
2024-05-26 23:51:01 +0530

    few changes

--- models/BusLocation.js
+++ models/BusLocation.js
@@ -7,7 +7,8 @@ const busLocationSchema = new mongoose.Schema({
   name: { type: String, required: true },
   route: { type: String, required: true },
   routes: [{ type: String }], // Array of strings for multiple stops
-  status: { type: String, required: true }
+  status: { type: String, required: true },
+  stopsCompleted: { type: Number, required: true, default: 0 }  // Default to 0 if not specified
 });
 
 const BusLocation = mongoose.model('BusLocation', busLocationSchema);
